7f625b0de48f59a99761053e28c31df762e15604
Neg.- <neg90.ng@gmail.com>
2015-05-28 05:47:36 -0300

    base de datos jesus lloron

---
+++ modelo/CE.sql
@@ -0,0 +1,100 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 28-05-2015 a las 05:46:56
+-- Versión del servidor: 5.5.43-0ubuntu0.14.04.1
+-- Versión de PHP: 5.5.9-1ubuntu4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `CE`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `permisos`
+--
+
+CREATE TABLE IF NOT EXISTS `permisos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `csocio` tinyint(1) NOT NULL,
+  `rsocio` tinyint(1) NOT NULL,
+  `usocio` tinyint(1) NOT NULL,
+  `dsocio` tinyint(1) NOT NULL,
+  `cmedidor` tinyint(1) NOT NULL,
+  `rmedidor` tinyint(1) NOT NULL,
+  `umedidor` tinyint(1) NOT NULL,
+  `dmedidor` tinyint(1) NOT NULL,
+  `cci` tinyint(1) NOT NULL,
+  `rci` tinyint(1) NOT NULL,
+  `uci` tinyint(1) NOT NULL,
+  `dci` tinyint(1) NOT NULL,
+  `crol` tinyint(1) NOT NULL,
+  `rrol` tinyint(1) NOT NULL,
+  `urol` tinyint(1) NOT NULL,
+  `drol` tinyint(1) NOT NULL,
+  `cusuario` tinyint(1) NOT NULL,
+  `rusuario` tinyint(1) NOT NULL,
+  `uusuario` tinyint(1) NOT NULL,
+  `dusuario` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
+CREATE TABLE IF NOT EXISTS `rol` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(10) NOT NULL,
+  `idpermisos` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idpermisos` (`idpermisos`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(10) NOT NULL,
+  `apellido` varchar(10) NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `activo` tinyint(1) NOT NULL,
+  `idrol` int(11) NOT NULL,
+  `correo` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idrol` (`idrol`),
+  KEY `idrol_2` (`idrol`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`idrol`) REFERENCES `rol` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
