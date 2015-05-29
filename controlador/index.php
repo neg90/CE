17fd500e215e87e6aff626fb870a398bbe4a2847
Alejandro Cabane <cabanehb@gmail.com>
2015-05-29 14:43:51 -0300

    login

--- controlador/index.php
+++ controlador/index.php
@@ -1,4 +1,15 @@
 <?php
-	include_once('publico.php');
-	ControladorPublico::Principal(); //LLAMO AL METODO PRINCIPAL DEL CONTROLADOR PUBLICO
+
+		if (empty(htmlentities($_GET['aviso'])))
+			$_SESSION['error'] = 0;
+		elseif (htmlentities($_GET['aviso'] == 1))
+			$_SESSION['error'] = 1;
+		elseif (htmlentities($_GET['aviso'] == 2))
+			$_SESSION['error'] = 2;
+		elseif (htmlentities($_GET['aviso'] == 3))
+			$_SESSION['error'] = 3;
+		elseif (htmlentities($_GET['aviso'] == 4))
+			$_SESSION['error'] = 4;
+		
+		include_once('publico.php');
 ?>
---
+++ modelo/clases/PDOusuario.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once '../modelo/conexionDB.php';
+require_once 'usuario.php';
+
+class PDOusuario extends usuario{
+	
+
+	public function __construct ($id,$user,$email,$password,$activo,$id_rol,$id_persona){
+
+		parent::__construct($id,$user,$email,$password,$activo,$id_rol,$id_persona);
+	}
+
+	public static function verificarUser($unusername,$unapassword){
+		try {
+		$conexion = new conexion; //creo instancia de la conexion
+		}catch (PDOException $e){}
+		$consulta = $conexion->prepare('SELECT * FROM usuario WHERE username = :username and password = :password');
+		$consulta->bindParam(':username',$unusername);
+		$consulta->bindParam(':password',$unapassword);
+		$consulta->execute();
+		$existe=$consulta->fetch();
+		
+		if($existe==true){
+			return 1;
+		}else{
+			return 0;
+		}	
+			
+
+	}
+//revisar
+	public function buscarUser ($user,$password){
+		try {
+		$conexion = new conexion; //creo instancia de la conexion
+		}catch (PDOException $e){}
+		$consulta = $conexion->prepare('SELECT * FROM usuario WHERE username = :username and password = :password');
+		$consulta->execute();
+		$existe=$consulta->fetch();
+		
+
+	}
+
+
+	public static function modificar($user,$password){
+		try {
+		$conexion = new conexion; //creo instancia de la conexion
+		}catch (PDOException $e){}
+		$consulta = $conexion->prepare('UPDATE usuario SET username = :username AND password = :password  WHERE username = :username');
+		$consulta->bindParam(':username',$unusername);
+		$consulta->bindParam(':password',$unapassword);
+		$consulta->execute();
+		
+	}
+}
+?>
