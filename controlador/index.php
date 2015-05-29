3039fd15ca5c722cf1f810a2dc375050c2775bd4
Alejandro Cabane <cabanehb@gmail.com>
2015-05-29 15:43:42 -0300

    mensajes de error inicio de sesión

--- controlador/index.php
+++ controlador/index.php
@@ -1,15 +1,20 @@
 <?php
-
+	session_start();
+	if (htmlentities(isset($_GET['aviso']))){
 		if (empty(htmlentities($_GET['aviso'])))
 			$_SESSION['error'] = 0;
-		elseif (htmlentities($_GET['aviso'] == 1))
+		elseif (htmlentities($_GET['aviso'] == 1)){
 			$_SESSION['error'] = 1;
+		}
 		elseif (htmlentities($_GET['aviso'] == 2))
 			$_SESSION['error'] = 2;
 		elseif (htmlentities($_GET['aviso'] == 3))
 			$_SESSION['error'] = 3;
 		elseif (htmlentities($_GET['aviso'] == 4))
 			$_SESSION['error'] = 4;
-		
-		include_once('publico.php');
+	}
+	else {
+		$_SESSION['error'] = 0;
+	}
+	include_once('publico.php');
 ?>
--- controlador/publico.php
+++ controlador/publico.php
@@ -1,10 +1,10 @@
 <?php
-
+	$error = $_SESSION['error'];
 	require_once '../vendor/twig/twig/lib/Twig/Autoloader.php';
 	Twig_Autoloader::register();
 	$loader = new Twig_Loader_Filesystem('../vista');
 	$twig = new Twig_Environment($loader, array('debug' => 'false'));//'cache' => '../cache',')); 
 	$template = $twig->loadTemplate('login.html.twig');
-	echo $template->render(array());
+	echo $template->render(array('error' => $error));
 
 ?>
