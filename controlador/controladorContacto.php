910efe13181dd3cf139b3502bd5ce94114c735a3
Neg.- <neg90.ng@gmail.com>
2015-05-29 23:17:43 -0300

    vista listar contacto

--- controlador/controladorContacto.php
+++ controlador/controladorContacto.php
@@ -2,19 +2,28 @@
 
 	require_once('../modelo/conexionDB.php');
 	require_once('../modelo/PDO/PDOContacto.php');
+	require_once '../vendor/twig/twig/lib/Twig/Autoloader.php';
 	
 
 class controladorContacto {
 
 	static function alta(){
 
+	
+
+		}
+		
+
+	static function listar(){
+
 		Twig_Autoloader::register();
 	  	$loader = new Twig_Loader_Filesystem('../vista');
 	  	$twig = new Twig_Environment($loader, array('cache' => '../cache','debug' => 'false')); 
 		
 		
-		$template = $twig->loadTemplate('contacto.html.twig');
-		echo $template->render(array('Correo'=>$Correo,'Ubicacion'=>$Ubicacion,'Telefono'=>$Telefono));
+		
+		$template = $twig->loadTemplate('contacto/listarContacto.html.twig');
+		echo $template->render(array());
 
 		}
 		
