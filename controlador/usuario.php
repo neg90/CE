78d411b2890d0a013b41a4a7cd8784c9e6981e9f
Alejandro Cabane <cabanehb@gmail.com>
2015-05-29 23:34:03 -0300

    db

--- controlador/usuario.php
+++ controlador/usuario.php
@@ -5,8 +5,7 @@ require_once '../modelo/clases/PDOusuario.php';
 
 
 static function listar(){
-		$NombreUsuario=$_SESSION['user'];
-  		$rol=($_SESSION['rol']);
+		$user=$_SESSION['user'];
 		Twig_Autoloader::register();
 		$loader = new Twig_Loader_Filesystem('../vista');
 		$twig = new Twig_Environment($loader, array(
@@ -14,40 +13,10 @@ static function listar(){
 		'debug' => 'false'
 		));
 
-		$Usuarios=PDOusuario::listarUsuarios();
-
-		if (!empty($Usuarios)) {
-			for ($i=0; $i<count($Usuarios);$i++){
-				$NombreRol=PDOrol::obtenerNombreRol($Usuarios[$i][5]);
-				$Persona=PDOpersona::buscarPersonaPorID($Usuarios[$i][6]);
-
-				/*var_dump($Persona);
-				die (
-					'Usuario: ' . $Usuarios[$i][0] . '</br>' .
-					'Nombre: ' . $Persona[1] . '</br>' .
-					'Apellido: ' . $Persona[2] . '</br>' .
-					'DNI: ' . $Persona[3] . '</br>' .
-					'Email: ' . $Usuarios[$i][1] . '</br>' .
-					'NombreRol: ' . $NombreRol . '</br>' .
-					'Activo: ' . $Usuarios[$i][3]
-					);
-				*/
-			
-
-				$ListaUsuarios[$i]=Array('id'=>$Usuarios[$i][4],'Usuario'=>$Usuarios[$i][0],'Nombre'=>$Persona[1],
-										'Apellido'=>$Persona[2],'DNI'=>$Persona[3],
-										'Email'=>$Usuarios[$i][1],'NombreRol'=>$NombreRol[0],
-										'Activo'=>$Usuarios[$i][3]);
-			}
-		}
-		else {
-			$Usuarios=null;
-		}
-
-
+		$ListaUsuarios=PDOusuario::listarUsuarios();
 
 		$template = $twig->loadTemplate('Usuario/listar.html.twig');
-		echo $template->render(array('rol'=>$rol,'NombreUsuario'=>$NombreUsuario,'ListaUsuarios'=>$ListaUsuarios));	
+		echo $template->render(array('user'=>$user,'ListaUsuarios'=>$ListaUsuarios));	
 	}
 
 ?>
--- modelo/CE.sql
+++ modelo/CE.sql
@@ -2,10 +2,10 @@
 -- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
--- Servidor: localhost
--- Tiempo de generación: 29-05-2015 a las 21:15:56
--- Versión del servidor: 5.5.43-0ubuntu0.14.04.1
--- Versión de PHP: 5.5.9-1ubuntu4.9
+-- Host: localhost
+-- Generation Time: May 29, 2015 at 11:33 PM
+-- Server version: 5.5.43-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de datos: `CE`
+-- Database: `CE`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `categoria`
+-- Table structure for table `categoria`
 --
 
 CREATE TABLE IF NOT EXISTS `categoria` (
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `categoria` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `contacto`
+-- Table structure for table `contacto`
 --
 
 CREATE TABLE IF NOT EXISTS `contacto` (
@@ -54,7 +54,7 @@ CREATE TABLE IF NOT EXISTS `contacto` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `contactoempresa`
+-- Table structure for table `contactoempresa`
 --
 
 CREATE TABLE IF NOT EXISTS `contactoempresa` (
@@ -69,7 +69,7 @@ CREATE TABLE IF NOT EXISTS `contactoempresa` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `empresa`
+-- Table structure for table `empresa`
 --
 
 CREATE TABLE IF NOT EXISTS `empresa` (
@@ -93,7 +93,7 @@ CREATE TABLE IF NOT EXISTS `empresa` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `medidor`
+-- Table structure for table `medidor`
 --
 
 CREATE TABLE IF NOT EXISTS `medidor` (
@@ -112,7 +112,7 @@ CREATE TABLE IF NOT EXISTS `medidor` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `medidorempresa`
+-- Table structure for table `medidorempresa`
 --
 
 CREATE TABLE IF NOT EXISTS `medidorempresa` (
@@ -127,7 +127,7 @@ CREATE TABLE IF NOT EXISTS `medidorempresa` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `permisos`
+-- Table structure for table `permisos`
 --
 
 CREATE TABLE IF NOT EXISTS `permisos` (
@@ -153,12 +153,19 @@ CREATE TABLE IF NOT EXISTS `permisos` (
   `uusuario` tinyint(1) NOT NULL,
   `dusuario` tinyint(1) NOT NULL,
   PRIMARY KEY (`idpermiso`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `permisos`
+--
+
+INSERT INTO `permisos` (`idpermiso`, `csocio`, `rsocio`, `usocio`, `dsocio`, `cmedidor`, `rmedidor`, `umedidor`, `dmedidor`, `cci`, `rci`, `uci`, `dci`, `crol`, `rrol`, `urol`, `drol`, `cusuario`, `rusuario`, `uusuario`, `dusuario`) VALUES
+(5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `rol`
+-- Table structure for table `rol`
 --
 
 CREATE TABLE IF NOT EXISTS `rol` (
@@ -167,12 +174,19 @@ CREATE TABLE IF NOT EXISTS `rol` (
   `idpermisos` int(11) NOT NULL,
   PRIMARY KEY (`idrol`),
   KEY `idpermisos` (`idpermisos`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `rol`
+--
+
+INSERT INTO `rol` (`idrol`, `nombre`, `idpermisos`) VALUES
+(6, 'root', 5);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `rubro`
+-- Table structure for table `rubro`
 --
 
 CREATE TABLE IF NOT EXISTS `rubro` (
@@ -185,7 +199,7 @@ CREATE TABLE IF NOT EXISTS `rubro` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuario`
+-- Table structure for table `usuario`
 --
 
 CREATE TABLE IF NOT EXISTS `usuario` (
@@ -198,52 +212,54 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `correo` varchar(30) NOT NULL,
   `idrol` int(11) NOT NULL,
   PRIMARY KEY (`idusuario`),
-  KEY `idrol` (`idrol`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  KEY `idrol` (`idrol`),
+  KEY `idrol_2` (`idrol`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
--- Volcado de datos para la tabla `usuario`
+-- Dumping data for table `usuario`
 --
 
 INSERT INTO `usuario` (`idusuario`, `nombre`, `apellido`, `username`, `password`, `activo`, `correo`, `idrol`) VALUES
-(1, 'asd', 'asd', 'asd', 'asd', 0, '', 0);
+(2, 'Alejandro', 'Cabane', 'qw3r7y', 'kapanga', 1, 'asd@asd.com', 6),
+(3, 'Nelson', 'Garrido', 'neg90', 'kapanga', 1, 'dsa@dsa.com', 6);
 
 --
--- Restricciones para tablas volcadas
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `contactoempresa`
+-- Constraints for table `contactoempresa`
 --
 ALTER TABLE `contactoempresa`
   ADD CONSTRAINT `contactoempresa_ibfk_2` FOREIGN KEY (`idempresa`) REFERENCES `empresa` (`idempresa`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `contactoempresa_ibfk_1` FOREIGN KEY (`idcontacto`) REFERENCES `contacto` (`idcontacto`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `empresa`
+-- Constraints for table `empresa`
 --
 ALTER TABLE `empresa`
   ADD CONSTRAINT `empresa_ibfk_2` FOREIGN KEY (`idcategoria`) REFERENCES `categoria` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `empresa_ibfk_1` FOREIGN KEY (`idrubro`) REFERENCES `rubro` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `medidorempresa`
+-- Constraints for table `medidorempresa`
 --
 ALTER TABLE `medidorempresa`
   ADD CONSTRAINT `medidorempresa_ibfk_2` FOREIGN KEY (`idempresa`) REFERENCES `empresa` (`idempresa`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `medidorempresa_ibfk_1` FOREIGN KEY (`idmedidor`) REFERENCES `medidor` (`idmedidor`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `permisos`
---
-ALTER TABLE `permisos`
-  ADD CONSTRAINT `permisos_ibfk_1` FOREIGN KEY (`idpermiso`) REFERENCES `rol` (`idrol`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `rol`
+-- Constraints for table `rol`
 --
 ALTER TABLE `rol`
-  ADD CONSTRAINT `rol_ibfk_1` FOREIGN KEY (`idrol`) REFERENCES `usuario` (`idrol`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `rol_ibfk_1` FOREIGN KEY (`idpermisos`) REFERENCES `permisos` (`idpermiso`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`idrol`) REFERENCES `rol` (`idrol`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
